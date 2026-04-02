ccb281e407f128767bdb1973474751848417ab77
mujib77 <mujibkhanpathan6@gmail.com>
2026-04-02 18:30:22 +0530

    Cors changes

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const pool = require('./db');
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = 3000;
 
 app.use(express.json())
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
@@ -54,6 +56,7 @@ app.delete('/deleteuser/:id', async(req, res) => {
   const { id } = req.params;
   await pool.query('DELETE FROM users WHERE id = $1', [id])
   res.json({ message: "User deleted" })
+
 })
 
 app.listen(port, () => {
